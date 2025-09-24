6cf83f2a2817c31c400ece3d66d0e2c730db7781
md ismail <mdismail85773@gmail.com>
2025-09-24 19:00:12 +0800

    login in with google function added

--- src/components/homelayout/SocialLogin.jsx
+++ src/components/homelayout/SocialLogin.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { use } from 'react';
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { AuthContext } from '../../provider/AuthProvider';
 
 const SocialLogin = () => {
+    const {loginWithGoogle} = use(AuthContext);
+    // console.log(user);
     return (
         <div>
             <h2 className='font-bold mb-5'>Login With</h2>
             <div className='space-y-3'>
-                <button className='btn w-full btn-outline btn-secondary text-xs sm:text-sm md:text-base lg:text-lg whitespace-nowrap'>
+                <button  onClick={loginWithGoogle} className='btn w-full btn-outline btn-secondary text-xs sm:text-sm md:text-base lg:text-lg whitespace-nowrap'>
                     <FcGoogle size={18} className="" />
                     Login with Google
                 </button>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,21 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
 import app from '../firebase/Firebase.config';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+
+const provider = new GoogleAuthProvider(); 
 
 const auth = getAuth(app);// (app) come from firebase.config file (getAuth) from firebase
 export const AuthContext = createContext(); // create context 
+
 
 const AuthProvider = ({children}) => {
     const [user,  setUser] = useState(null);
     const [loading, setLoading] = useState(true);   // for user loading
     // console.log( loading ,user)
-    // register function
+    // register function with email and password
     const createUser =(email, password)=>{
         // console.log(email,password)
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     };
+
+    // log in register  with google
+    const loginWithGoogle =() =>{
+        signInWithPopup(auth,provider )
+        .then(result =>{
+            // console.log(result);
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }; 
 
     // user observer function for takeing current user 
     useEffect(()=>{
@@ -53,7 +67,8 @@ const AuthProvider = ({children}) => {
         logIn ,         // pass to login page for login
         loading,
         setLoading,
-        updateUser   // pass to register page for update user
+        updateUser,     // pass to register page for update user
+        loginWithGoogle,  // pass to social login page for google login 
     }
     return <AuthContext value={authData}>
         {children}
